3a8fdb3766a70bf0b7d9d99495ced81277354227
tofik <tofik.imscit21@gmail.com>
2026-04-06 20:04:35 +0530

    Images issue solving

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,31 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  domains: [
-    "127.0.0.1",
-    "localhost",
-    "3.109.240.161",
-    "www.iconperfumes.in",
-    "www.hidelifestyle.au",
-    "api.iconperfumes.in",
-    "www.test4.wardah.in",
-    "api.test4.wardah.in",
-    "api.iconperfumes.in",
-  ],
-},
+    unoptimized: true,
+    domains: [
+      "127.0.0.1",
+      "localhost",
+      "3.109.240.161",
+      "www.iconperfumes.in",
+      "www.hidelifestyle.au",
+      "api.iconperfumes.in",
+      "www.test4.wardah.in",
+      "api.test4.wardah.in",
+    ],
+  },
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
     IMAGE_URL: process.env.IMAGE_URL,
     STRAPI_API: process.env.STRAPI_API,
     BANNER_IMAGE_URL: process.env.BANNER_IMAGE_URL,
-    DOMAIN: process.env.DOMAIN
+    DOMAIN: process.env.DOMAIN,
   },
   trailingSlash: true,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["secure.payu.in", "www.iconperfumes.in"],
-    },
-  },
 };
 
 export default nextConfig;
