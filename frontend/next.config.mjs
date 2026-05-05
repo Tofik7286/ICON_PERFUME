9c10c3b86fad3c870a247457aaa2617c48969b52
tofik <tofik.imscit21@gmail.com>
2026-05-05 16:20:22 +0530

    done

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   },
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
-    IMAGE_URL: process.env.IMAGE_URL,
+    IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL,
     STRAPI_API: process.env.STRAPI_API,
     BANNER_IMAGE_URL: process.env.BANNER_IMAGE_URL,
     DOMAIN: process.env.DOMAIN,
